e41487f1cdda31634f7628fe70f527ad2501090b
Slaykha <kadirengin2000@hotmail.com>
2023-03-31 21:05:31 +0300

    add player animations and movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,65 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    public float collisionOffSet = 0.05f;
+    public float collisionOffSet = 0.01f;
     public ContactFilter2D movementFilter;
 
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rBody;
+    Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate(){
         if(movementInput != Vector2.zero){
-            int count = rBody.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffSet);
+            bool success = TryMove(movementInput);
 
-            if(count == 0) {
-                rBody.MovePosition(rBody.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
 
             }
+
+            if (!success)
+            {
+                TryMove(new Vector2(0, movementInput.y));
+            }
+
+            animator.SetFloat("moveX", movementInput.x);
+            animator.SetFloat("moveY", movementInput.y);
+            animator.SetBool("isMoving", success);
         }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        spriteRenderer.flipX = false;
+        if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction == Vector2.zero) { return false; }
+        int count = rBody.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffSet);
+
+        if (count == 0)
+        {
+            rBody.MovePosition(rBody.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        return false;
     }
 
     void OnMove(InputValue movementValue){
